82c2aadd5a2d14897479e8dd23f02bead9eb042d
shakogit <76178524+shakogit@users.noreply.github.com>
2026-09-06 20:12:06 +0400

    Increment cache version to v55
    Updated cache name to version v55.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'visual-german-v54'; // ვერსია გავზარდეთ v2-ზე
+const CACHE_NAME = 'visual-german-v55'; // ვერსია გავზარდეთ v2-ზე
 
 // ყველა ფაილის სია, რომელიც უნდა დაკეშირდეს ოფლაინ მუშაობისთვის
 const CACHE_URLS = [
