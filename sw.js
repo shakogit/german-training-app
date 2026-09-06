3f182825f85ae8209e3a99a89a7b0f023358a8ca
shakogit <76178524+shakogit@users.noreply.github.com>
2026-09-06 21:24:04 +0400

    Increment cache version to v57
    Updated cache version from v56 to v57.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'visual-german-v56'; // ვერსია გავზარდეთ v2-ზე
+const CACHE_NAME = 'visual-german-v57'; // ვერსია გავზარდეთ v2-ზე
 
 // ყველა ფაილის სია, რომელიც უნდა დაკეშირდეს ოფლაინ მუშაობისთვის
 const CACHE_URLS = [
