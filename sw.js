f8aa2975f44189821757cd34eed426c35d2aa976
shakogit <76178524+shakogit@users.noreply.github.com>
2026-09-13 12:39:18 +0400

    Change CACHE_NAME to visual-german-v59
    Updated cache version to v59 for offline functionality.

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'visual-german-v58'; // ვერსია გავზარდეთ v2-ზე
+const CACHE_NAME = 'visual-german-v59'; // ვერსია გავზარდეთ v2-ზე
 
 // ყველა ფაილის სია, რომელიც უნდა დაკეშირდეს ოფლაინ მუშაობისთვის
 const CACHE_URLS = [
