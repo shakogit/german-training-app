f278ea7b1241414a8c9b19365ef753e71bdb214a
Shakogit <shakoinbox@.com>
2026-09-18 16:14:28 +0400

    index_73

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'visual-german-v72'; // ვერსია გავზარდეთ v2-ზე
+const CACHE_NAME = 'visual-german-v73'; // ვერსია გავზარდეთ v2-ზე
 
 // ყველა ფაილის სია, რომელიც უნდა დაკეშირდეს ოფლაინ მუშაობისთვის
 const CACHE_URLS = [
